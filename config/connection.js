292c8853c4916ace25bc65011d02971abfa93c56
eliabn <30064396@tafe.wa.edu.au>
2023-09-20 17:09:46 +0930

    Connection.js file completed

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,19 @@
-// Import sequelize
+// Import the Sequelize library
 const Sequelize = require('sequelize');
+
+// Load environment variables from .env file (using dotenv library)
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
+
+// Export the sequelize instance to be used in application
+module.exports = sequelize;
