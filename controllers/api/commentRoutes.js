f5db539691e3ec2d6c1b657cbf45b85f81ca2afd
eliabn <30064396@tafe.wa.edu.au>
2023-09-21 12:35:10 +0930

    Installed node modules and GET all posts done

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,7 +1,8 @@
 // TODO: WHEN I enter a comment and click on the submit button while signed in
 // THEN the comment is saved and the post is updated to display the comment, the comment creator’s username, and the date created
 
-
+//THEN I am prompted to enter my username and password
 // THEN I am signed out of the site
+
 // WHEN I am idle on the site for more than a set time
 // THEN I am able to view posts and comments but I am prompted to log in again before I can add, update, or delete posts
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,41 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../models');
+//const withAuth = require('../utils/auth');
+
+
 // TODO:
 // WHEN I visit the site for the first time
 // THEN I am presented with the homepage, which includes existing blog posts if any have been posted; navigation links for the homepage and the dashboard; and the option to log in
+
+// GET all posts
+router.get('/', async (req, res) => {
+    try {
+        // Get all posts and JOIN with user data
+        const postsData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        // // Testing for insomnia
+        res.json(productData);
+
+        // Serialize data so the template can read it
+        const posts = postsData.map((posts) => posts.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            posts,
+            //logged_in: req.session.logged_in 
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 
 // WHEN I click on the homepage option
