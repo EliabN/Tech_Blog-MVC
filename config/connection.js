3d8da889ad0ec2a24a2ba38a70e0fc65aed20c57
eliabn <30064396@tafe.wa.edu.au>
2023-10-15 00:48:55 +1030

    Change connection

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,22 @@ const Sequelize = require('sequelize');
 // Load environment variables from .env file (using dotenv library)
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  }
-);
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
 
 // Export the sequelize instance to be used in application
 module.exports = sequelize;
